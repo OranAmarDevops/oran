7a44b5aa4959d1f0e4a6f7bf855ec7d67ff0a3b9
OranAmarDevops <019oran019@gmail.com>
2025-05-24 10:06:07 +0300

    Update RapidAPI_6.py

--- 6_argparse+rapidAPI/RapidAPI_6.py
+++ 6_argparse+rapidAPI/RapidAPI_6.py
@@ -4,7 +4,7 @@ def search_movies(pattern):
     url = "https://movie-database-alternative.p.rapidapi.com/"
     querystring = {"s": pattern, "r": "json", "page" : "1"}
     headers = {
-		"x-rapidapi-key": "7c41e491fdmsha522b68bf01bca9p148259jsn06fadbae9d0c",
+		"x-rapidapi-key": "my_key",
 		"x-rapidapi-host": "movie-database-alternative.p.rapidapi.com"
 	}
     response = requests.get(url, headers=headers, params=querystring)
