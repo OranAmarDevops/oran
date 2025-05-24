d0021378536a7781f13fafd67cd46a5c21e0d708
OranAmarDevops <019oran019@gmail.com>
2025-05-24 10:18:56 +0300

    Update movie_db.py

--- 10_html+flask/movie_db.py
+++ 10_html+flask/movie_db.py
@@ -48,7 +48,7 @@ def movie_search(pattern):
     querystring = {"s": pattern, "r": "json", "page": "1"}
 
     headers = {
-        "x-rapidapi-key": "7c41e491fdmsha522b68bf01bca9p148259jsn06fadbae9d0c",
+        "x-rapidapi-key": "my_key",
         "x-rapidapi-host": "movie-database-alternative.p.rapidapi.com"
     }
 
